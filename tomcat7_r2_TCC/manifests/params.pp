37ab332d1d925cac2a9c1e3a45c4fbf69fed63c0
leandrobs1984 <leandrobs1984@gmail.com>
2015-06-23 00:04:15 -0300

    Revert "Add Modulo Tomcat"
    This reverts commit c9c4a1ea8497dd62684338ad9cbe6611619c512a.

--- tomcat7_r2_TCC/manifests/params.pp
+++
@@ -1,7 +0,0 @@
-class tomcat7_r2::params {
-	$tomcat_user = "tomcat" 
-	$java_opts = "-Xms512m -Xmx1000m -XX:MaxPermSize=256M -XX:PermSize=128M"
-	$tomcat_partition = "/tomcat"	
-	$java_home = "/opt/java"
-        $env = undef
-}
